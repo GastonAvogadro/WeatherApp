a72dd4dbaadd8dc138988216b1956b9229448731
Gaston Avogadro <gaston.avogadro@gmail.com>
2022-08-22 18:03:10 -0300

    temp min y max se agrego

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const apiKey = '5e900ba172cd8945a01eb43f95d8bf7a',
     temperatura = document.querySelector('.temperatura'),
     btnBusqueda = document.querySelector('.btnBusqueda'),
     ciudadIngresada = document.querySelector('.inputBusqueda'),
-    ultimaCiudad = document.querySelector('.ultimaCiudad');
+    ultimaCiudad = document.querySelector('.ultimaCiudad'),
+    tempMin = document.querySelector('.tempMin'),
+    tempMax = document.querySelector('.tempMax');
 
 if ('ciudadGuardada' in localStorage) {
     ultimaCiudad.innerHTML = JSON.parse(localStorage.getItem('ciudadGuardada'));
@@ -13,6 +15,8 @@ if ('ciudadGuardada' in localStorage) {
 function crearCiudad(data) {
     ciudad.innerHTML = `${data.name}, ${data.sys.country}`;
     temperatura.innerHTML = Math.round(data.main.temp) + ' °c';
+    tempMin.innerHTML = `min ${Math.round(data.main.temp_min)} °c`;
+    tempMax.innerHTML = `max ${Math.round(data.main.temp_max)} °c`;
     icono.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png">`;
     ultimaCiudad.innerHTML = `${data.name}, ${data.sys.country}`;
     localStorage.setItem('ciudadGuardada', JSON.stringify(`${data.name}, ${data.sys.country}`));
@@ -32,20 +36,20 @@ const buscarCiudad = () => {
             .catch(() => {
                 ciudad.innerHTML = 'Ingrese una ciudad válida';
                 temperatura.innerHTML = '';
+                tempMin.innerHTML = '';
+                tempMax.innerHTML = '';
                 icono.innerHTML = '';
+                document.body.style.backgroundImage = `url('../img/default.jpg')`;
             });
     }
 };
 
 btnBusqueda.addEventListener('click', () => {
-    if (ciudadIngresada.value != '') {
-        buscarCiudad();
-    }
+    buscarCiudad();
 });
+
 ciudadIngresada.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter' && ciudadIngresada.value != '') {
-        buscarCiudad();
-    }
+    event.key === 'Enter' && buscarCiudad();
 });
 
 ultimaCiudad.addEventListener('click', () => {
