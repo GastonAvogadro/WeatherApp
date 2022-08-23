ded328be9c6f9165bf3a6a6156f5cafed77a962c
Gaston Avogadro <gaston.avogadro@gmail.com>
2022-08-23 17:01:58 -0300

    historial y storage

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-const apiKey = '5e900ba172cd8945a01eb43f95d8bf7a',
-    ciudad = document.querySelector('.ciudad'),
+// ELEMENTOS DEL DOM
+
+const ciudad = document.querySelector('.ciudad'),
     icono = document.querySelector('.icono'),
     temperatura = document.querySelector('.temperatura'),
     btnBusqueda = document.querySelector('.btnBusqueda'),
     ciudadIngresada = document.querySelector('.inputBusqueda'),
-    ultimaCiudad = document.querySelector('.ultimaCiudad'),
+    ultimaCiudad = document.querySelectorAll('.ultimaCiudad'),
     tempMin = document.querySelector('.tempMin'),
-    tempMax = document.querySelector('.tempMax');
+    tempMax = document.querySelector('.tempMax'),
+    historialCiudades = document.querySelector('.historialCiudades');
 
-if ('ciudadGuardada' in localStorage) {
-    ultimaCiudad.innerHTML = JSON.parse(localStorage.getItem('ciudadGuardada'));
+// HISTORIAL DE CIUDADES
+
+ciudadIngresada.value = '';
+
+const ciudadesGuardadas = [];
+
+const datosLocalStorage = JSON.parse(localStorage.getItem('historialCiudades'));
+
+if (datosLocalStorage != null) {
+    for (const ciudad of datosLocalStorage) {
+        ciudadesGuardadas.push(ciudad);
+    }
 }
+
+for (const ciudad of ciudadesGuardadas) {
+    const li = `<li class="ultimaCiudad">${ciudad}</li>`;
+    historialCiudades.innerHTML += li;
+}
+
+// CREACION DE CIUDAD
 
 function crearCiudad(data) {
     ciudad.innerHTML = `${data.name}, ${data.sys.country}`;
@@ -19,16 +38,44 @@ function crearCiudad(data) {
     tempMax.innerHTML = `max ${Math.round(data.main.temp_max)} °c`;
     icono.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png">`;
     ultimaCiudad.innerHTML = `${data.name}, ${data.sys.country}`;
-    localStorage.setItem('ciudadGuardada', JSON.stringify(`${data.name}, ${data.sys.country}`));
+
     if (data.weather[0].icon.includes('d')) {
-        document.body.style.backgroundImage = `url('../img/dia.jpg')`;
+        document.body.style.backgroundImage = `url('./img/dia.jpg')`;
     } else {
-        document.body.style.backgroundImage = `url('../img/noche.jpg')`;
+        document.body.style.backgroundImage = `url('./img/noche.jpg')`;
     }
+
+    // GUARDADO EN HISTORIAL
+
+    if (ciudadesGuardadas.length < 5 && !ciudadesGuardadas.includes(ciudad.innerHTML)) {
+        ciudadesGuardadas.unshift(ciudad.innerHTML);
+    } else if (ciudadesGuardadas.length === 5 && !ciudadesGuardadas.includes(ciudad.innerHTML)) {
+        ciudadesGuardadas.pop();
+        ciudadesGuardadas.unshift(ciudad.innerHTML);
+    } else {
+        let posicion = ciudadesGuardadas.indexOf(ciudad.innerHTML);
+        ciudadesGuardadas.splice(posicion, 1);
+        ciudadesGuardadas.unshift(ciudad.innerHTML);
+    }
+
+    historialCiudades.innerHTML = 'Historial de ciudades';
+
+    for (const ciudad of ciudadesGuardadas) {
+        const li = `<li class="ultimaCiudad">${ciudad}</li>`;
+        historialCiudades.innerHTML += li;
+    }
+
+    // GUARDADO EN STORAGE
+
+    localStorage.clear();
+    localStorage.setItem('historialCiudades', JSON.stringify(ciudadesGuardadas));
 }
+
+// CONSULTA A LA API
 
 const buscarCiudad = () => {
     if (ciudadIngresada.value != '') {
+        const apiKey = '5e900ba172cd8945a01eb43f95d8bf7a';
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudadIngresada.value}&appid=${apiKey}&units=metric`;
         fetch(url)
             .then((response) => response.json())
@@ -39,10 +86,12 @@ const buscarCiudad = () => {
                 tempMin.innerHTML = '';
                 tempMax.innerHTML = '';
                 icono.innerHTML = '';
-                document.body.style.backgroundImage = `url('../img/default.jpg')`;
+                document.body.style.backgroundImage = `url('./img/default.jpg')`;
             });
     }
 };
+
+// BOTONES Y ACCIONES
 
 btnBusqueda.addEventListener('click', () => {
     buscarCiudad();
@@ -52,7 +101,9 @@ ciudadIngresada.addEventListener('keypress', (event) => {
     event.key === 'Enter' && buscarCiudad();
 });
 
-ultimaCiudad.addEventListener('click', () => {
-    ciudadIngresada.value = ultimaCiudad.innerHTML;
-    buscarCiudad();
+document.body.addEventListener('click', function (e) {
+    if (e.target.classList.contains('ultimaCiudad')) {
+        ciudadIngresada.value = e.target.innerHTML;
+        buscarCiudad();
+    }
 });
